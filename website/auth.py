aef545bafe5a31071052f7adf7f8515aa672246c
denisborbasianhaki <denisborbasianhaki@gmail.com>
2021-10-03 20:05:36 -0300

    Atualização do sistema de login

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,26 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth =  Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in sucessfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     data = request.form
     print(data)
     return  render_template('login.html')
@@ -16,6 +33,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+
         if len(email) < 4 :
             flash('Email must be greater than 3 characters.', category='error')
         elif len(username) < 2:
@@ -25,6 +46,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return  render_template('sign_up.html')
