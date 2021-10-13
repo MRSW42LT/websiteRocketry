d272f220d13a70e969f39d130fde8402b82838dc
denisborbasianhaki <denisborbasianhaki@gmail.com>
2021-10-12 21:24:14 -0300

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -43,8 +43,8 @@ def sign_up():
             flash('Username must be greater than 1 characters.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match. ', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        elif len(password1) < 4:
+            flash('Password must be at least 4 characters.', category='error')
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
